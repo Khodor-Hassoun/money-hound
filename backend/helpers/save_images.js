9a21a76653943916ecde9132ac6ce4d6db4d4fd3
Khodor Hassoun <khodorhassoun@outlook.com>
2022-10-28 14:22:45 +0300

    Backend: create method to save image

---
+++ backend/helpers/save_images.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+function saveImage(baseImage) {
+  /*path of the folder where your project is saved. (In my case i got it from config file, root path of project).*/
+  const uploadPath = `C:/Users/khodor/Code/SeFactory/Webdev-SeF/LinkedOut/backend`;
+  //path of folder where you want to save the image.
+  const localPath = `${uploadPath}/uploads/images/`;
+  //Find extension of file
+  const ext = baseImage.substring(
+    baseImage.indexOf("/") + 1,
+    baseImage.indexOf(";base64")
+  );
+  const fileType = baseImage.substring("data:".length, baseImage.indexOf("/"));
+  //Forming regex to extract base64 data of file.
+  const regex = new RegExp(`^data:${fileType}\/${ext};base64,`, "gi");
+  //Extract base64 data.
+  const base64Data = baseImage.replace(regex, "");
+  const rand = Math.ceil(Math.random() * 1000);
+  //Random photo name with timeStamp so it will not overide previous images.
+  const filename = `Photo_${Date.now()}_${rand}.${ext}`;
+
+  //Check that if directory is present or not.
+  if (!fs.existsSync(`${uploadPath}/uploads/`)) {
+    fs.mkdirSync(`${uploadPath}/uploads/`);
+  }
+  if (!fs.existsSync(localPath)) {
+    fs.mkdirSync(localPath);
+  }
+  fs.writeFileSync(localPath + filename, base64Data, "base64");
+  return { filename, localPath };
+  // return filename;
+}
+
+module.exports = saveImage;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const app = express();
 require("dotenv").config();
+const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const cors = require("cors");
+const app = express();
 const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Listening on ${process.env.SERVER_PORT}`);
