63921d93f07e7d6798cc76d766d7b5c9ea6affb5
Khodor Hassoun <khodorhassoun@outlook.com>
2022-10-28 17:26:29 +0300

    Backend: validate user before insertion

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,6 +3,18 @@ const prisma = new PrismaClient();
 
 const signUp = async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
+  //   validate if email exists
+  const userTest = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+  if (!email || userTest) {
+    res.status(400).json({ message: " Email Something went wrong" });
+    return;
+  }
+
+  //   Create user
   const user = await prisma.user.create({
     data: {
       firstname: firstname,
