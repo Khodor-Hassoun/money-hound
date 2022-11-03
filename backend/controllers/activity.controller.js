c12472d91c115b90b73d50df9683875ba9d73c1d
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-03 21:36:36 +0200

    Backend; add activity

--- backend/controllers/activity.controller.js
+++ backend/controllers/activity.controller.js
@@ -1,2 +1,20 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+const addActivity = async (req, res) => {
+  let { objective, money, end_date } = req.body;
+  const projectId = req.params.id;
+  let endDate = new Date(end_date);
+  //   res.json({ endDate, objective, projectId });
+  //   return;
+  const activity = await prisma.activity.create({
+    data: {
+      objective: objective,
+      money: parseInt(money),
+      end_date: endDate,
+      projectId: parseInt(projectId),
+    },
+  });
+  res.json(activity);
+};
+module.exports = { addActivity };
--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -6,6 +6,7 @@ const {
   getProjects,
   updateProject,
 } = require("../controllers/project.controller");
+const { addActivity } = require("../controllers/activity.controller");
 const { Router } = require("express");
 const router = Router();
 
@@ -13,5 +14,7 @@ router.post("/", authMiddleware, ownerMiddleware, addProject);
 router.get("/", authMiddleware, ownerMiddleware, getProjects);
 router.get("/:id", authMiddleware, getProject);
 router.put("/", authMiddleware, updateProject);
+
+router.post("/:id/add", authMiddleware, addActivity);
 
 module.exports = router;
