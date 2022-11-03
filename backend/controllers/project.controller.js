7abc197f6918531c0b3036f418aa4b83c2ef164a
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-03 12:52:34 +0200

    Backend: add project

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -2,7 +2,21 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const addProject = async (req, res) => {
-  const {} = req.body;
+  const { customer_email, project_name, budget, deadline, managerId } =
+    req.body;
+  const { id } = req.company;
+  let customerDeadline = new Date(deadline);
+  const project = await prisma.project.create({
+    data: {
+      project_name: project_name,
+      customerId: customer_email,
+      companyId: id,
+      deadline: customerDeadline,
+      budget: budget,
+      managerId: parseInt(managerId),
+      project_phase_id: 1,
+    },
+  });
 };
 const getProjects = async (req, res) => {};
 const getProject = async (req, res) => {};
