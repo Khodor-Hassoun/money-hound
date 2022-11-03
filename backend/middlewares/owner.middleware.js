2e28055a18cf8b0b9e0ef49ae7a695cb09381b7f
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-03 11:10:41 +0200

    Backend: update owner middleware

--- backend/middlewares/owner.middleware.js
+++ backend/middlewares/owner.middleware.js
@@ -1,13 +1,23 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const ownerMiddleware = async (req, res, next) => {
   console.log(
     "---------------------OWNERMIDDLEWARE------------------------------"
   );
   console.log(req.user);
-  console.log(
-    "--------------------/OWNERMIDDLEWARE------------------------------"
-  );
+  console.log("---------------------------------------");
   // return;
   if (req.user.user_type === 1) {
+    const company = await prisma.company.findFirst({
+      where: {
+        ownerId: parseInt(req.user.id),
+      },
+    });
+    req.company = company;
+    console.log(company);
+    console.log(
+      "--------------------/OWNERMIDDLEWARE------------------------------"
+    );
     next();
   } else {
     return res.status(401).json({ message: "Unauthorized" });
