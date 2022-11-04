bc402840092d50cea879b3f740c2188c026bb13d
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-04 20:43:31 +0200

    Frontend: update tailwind with custom colors

--- frontend/webapp/tailwind.config.js
+++ frontend/webapp/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      ming: "#026A75",
+      "vivid-tangerine": "#F69983",
+      beau: "#C8DAE4",
+      offWhite: "#F2F2F2",
+    },
     extend: {},
   },
   plugins: [],
