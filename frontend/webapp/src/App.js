dfc94c98536dffa118b1a16c1d1c3e22eb54e840
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-04 22:50:52 +0200

    Frontend: import company logo

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -1,24 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import logo from "./moneylogo.svg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl font-bold underline">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="bg-ming w-screen h-screen flex justify-center items-center">
+      <div className="bg-offWhite">
+        <div>
+          <img src={logo} alt="Logo" />
+        </div>
+        <div>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+        </div>
+      </div>
+    </body>
   );
 }
 
--- frontend/webapp/tailwind.config.js
+++ frontend/webapp/tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      ming: "#026A75",
-      "vivid-tangerine": "#F69983",
-      beau: "#C8DAE4",
-      offWhite: "#F2F2F2",
+    extend: {
+      colors: {
+        ming: "#026A75",
+        tangerine: "#F69983",
+        beau: "#C8DAE4",
+        offWhite: "#F2F2F2",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
