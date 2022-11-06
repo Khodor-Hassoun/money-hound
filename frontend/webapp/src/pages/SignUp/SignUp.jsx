74f3e746c019d2b1bf03c9499d81cc77899dde0e
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-06 15:22:43 +0200

    Frontend: get user and company data

--- frontend/webapp/src/pages/SignUp/SignUp.jsx
+++ frontend/webapp/src/pages/SignUp/SignUp.jsx
@@ -17,6 +17,7 @@ function SignUp() {
     const phoneRef = useRef("")
     const userData = new FormData()
     const companyData = new FormData()
+
     // const data = new FormData()
     // data.append('firstname', firstnameRef.current.value)
     // data.append("lastname", lastnameRef.current.value)
@@ -41,10 +42,24 @@ function SignUp() {
         companyData.append("capital", capitalRef.current.value)
         console.log(...userData, ...companyData)
     }
+    function logData() {
+        const data = {
+            firstname: firstnameRef.current.value,
+            lastname: lastnameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            name: nameRef.current.value,
+            company_email: companyEmailRef.current.value,
+            address: addressRef.current.value,
+            phone: phoneRef.current.value,
+            capital: capitalRef.current.value
+        }
+        console.log(data)
+    }
     return (
         <>
             {showModal ? (
-                <section className="bg-ming w-screen h-screen flex justify-center items-center">
+                <section className="bg-ming w-screen h-screen flex justify-center items-center z-10">
                     <div className="w-[410px] h-[430px] bg-building bg-center bg-cover bg-no-repeat brightness-75">
                         <div className="w-[24px] h-[24px] py-10 px-6">
                             <div className="bg-arrow w-[24px] h-[24px] cursor-pointer" onClick={() => setShowModal(false)} />
@@ -82,51 +97,50 @@ function SignUp() {
                                 <input type="text" id="phone" placeholder="01455678" className="border-black border-solid border rounded py-2 px-1" ref={phoneRef}></input>
                             </div>
                         </div>
-                        <button className="bg-tangerine text-white my-4 p-2 rounded-full w-full" onClick={logCompanyData}>REGISTER</button>
+                        <button className="bg-tangerine text-white my-4 p-2 rounded-full w-full" onClick={logData}>REGISTER</button>
 
                     </div>
                 </section>
-            ) : (
-                <section className="bg-ming w-screen h-screen flex justify-center items-center">
-                    {/* FORM SECTION */}
-                    <div className="bg-offWhite py-10 px-6 w-[410px]">
-                        {/* HEADER SECTION */}
-                        <div className="flex p-2">
-                            <span className="text-2xl"><Link to="/">&#10005;</Link></span>
-                            <div className="w-full">
-                                <h2 className="flex justify-center text-2xl">Personal information</h2>
-                            </div>
+            ) : (null)}
+            <section className="bg-ming w-screen h-screen flex justify-center items-center">
+                {/* FORM SECTION */}
+                <div className="bg-offWhite py-10 px-6 w-[410px]">
+                    {/* HEADER SECTION */}
+                    <div className="flex p-2">
+                        <span className="text-2xl"><Link to="/">&#10005;</Link></span>
+                        <div className="w-full">
+                            <h2 className="flex justify-center text-2xl">Personal information</h2>
                         </div>
-                        {/* LABELS AND INPUTS */}
-                        <div className="flex p-2 w-full">
-                            {/* FIRST NAME LAST NAME */}
-                            <div className="flex flex-col mr-0.5 w-2/4">
-                                <label htmlFor="firstname">Firstname</label>
-                                <input type="text" id="firstname" placeholder="John" ref={firstnameRef} className="border-black border-solid border rounded py-2 px-1"></input>
-                            </div>
-                            <div className="flex flex-col w-2/4">
-                                <label htmlFor="lastname">lastname</label>
-                                <input type="text" id="lastname" placeholder="Doe" ref={lastnameRef} className="border-black border-solid border rounded py-2 px-1"></input>
-                            </div>
-                        </div>
-                        {/* EMAIL */}
-                        <div className="flex flex-col p-2">
-                            <label htmlFor="email">Email</label>
-                            <input type="text" id="email" placeholder="john@outlook.com" ref={emailRef} className="border-black border-solid border rounded py-2 px-1"></input>
-                        </div>
-                        {/* PASSWORD */}
-                        <div className="flex flex-col p-2">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" ref={passwordRef} className="border-black border-solid border rounded py-2 px-1"></input>
-                        </div>
-                        <button className="bg-tangerine text-white my-4 p-2 rounded-full w-full" onClick={() => { logUserData(); setShowModal(true) }}>NEXT</button>
                     </div>
-                    {/* IMAGE SECTION */}
-                    <div className="w-[410px] h-[446px]">
-                        <img src={placeholder} alt='placeholder' className="h-full w-full bg-cover" />
+                    {/* LABELS AND INPUTS */}
+                    <div className="flex p-2 w-full">
+                        {/* FIRST NAME LAST NAME */}
+                        <div className="flex flex-col mr-0.5 w-2/4">
+                            <label htmlFor="firstname">Firstname</label>
+                            <input type="text" id="firstname" placeholder="John" ref={firstnameRef} className="border-black border-solid border rounded py-2 px-1"></input>
+                        </div>
+                        <div className="flex flex-col w-2/4">
+                            <label htmlFor="lastname">lastname</label>
+                            <input type="text" id="lastname" placeholder="Doe" ref={lastnameRef} className="border-black border-solid border rounded py-2 px-1"></input>
+                        </div>
                     </div>
-                </section>
-            )}
+                    {/* EMAIL */}
+                    <div className="flex flex-col p-2">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" id="email" placeholder="john@outlook.com" ref={emailRef} className="border-black border-solid border rounded py-2 px-1"></input>
+                    </div>
+                    {/* PASSWORD */}
+                    <div className="flex flex-col p-2">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" ref={passwordRef} className="border-black border-solid border rounded py-2 px-1"></input>
+                    </div>
+                    <button className="bg-tangerine text-white my-4 p-2 rounded-full w-full" onClick={() => { setShowModal(true) }}>NEXT</button>
+                </div>
+                {/* IMAGE SECTION */}
+                <div className="w-[410px] h-[446px]">
+                    <img src={placeholder} alt='placeholder' className="h-full w-full bg-cover" />
+                </div>
+            </section>
         </>
     )
 }
