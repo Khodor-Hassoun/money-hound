c16e4ba9307767825171d306a62702b5f7fdebdd
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-06 21:23:18 +0200

    Frontend: design company logo and name

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import LogIn from "./pages/LogIn/LogIn";
 import SignUp from "./pages/SignUp/SignUp";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LogIn />}></Route>
+        <Route path="/" element={<Navbar />}></Route>
+        <Route path="/login" element={<LogIn />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
--- frontend/webapp/src/components/Navbar.jsx
+++ frontend/webapp/src/components/Navbar.jsx
@@ -1,2 +1,27 @@
-function Navbar() { }
+import logo from "../resources/images/seanpollockPhYq704ffdAunsplash.jpg"
+import dots from "../resources/images/icons8-more-24.png"
+function Navbar() {
+    return (
+        <nav className="h-screen w-[400px] bg-ming text-white">
+            {/* COMPANY LOGO, NAME AND COMPANY SETTINGS */}
+            <div className="flex items-center w-full pt-6 justify-between px-4">
+                {/* COMPANY LOGO AND NAME */}
+                <div className="flex items-center w-2/3 ">
+                    {/* COMPANY LOGO WHITE BACKGROUND */}
+                    <div className="h-[60px] w-[60px] bg-white flex justify-center items-center rounded-xl">
+                        <div className="h-[50px] w-[50px] rounded-xl">
+                            <img src={logo} alt="logo" className="h-full w-full rounded-xl" />
+                        </div>
+                    </div>
+                    {/* COMPANY NAME */}
+                    <h2 className="text-xl ml-3">Company name</h2>
+                </div>
+                {/* COMPANY SETTINGS FOR OWNER */}
+                <div className="">
+                    <img src={dots} alt="options" />
+                </div>
+            </div>
+        </nav>
+    )
+}
 export default Navbar
