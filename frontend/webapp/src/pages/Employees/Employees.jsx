1f1a8c2c52d490cb3de0281b77eb6d9f499b84c9
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-07 17:21:22 +0200

    Frontend: design tables

--- frontend/webapp/src/pages/Employees/Employees.jsx
+++ frontend/webapp/src/pages/Employees/Employees.jsx
@@ -5,16 +5,16 @@ function Employees() {
         <section className="flex bg-offWhite pr-4">
             <Navbar />
             {/* EMPLOYEE CONTENT */}
-            <section className="flex-grow my-6">
+            <section className="flex-grow">
                 {/* HEADER SECTION TITLE NAME SEARCH BAR BUTTON */}
-                <header className="flex items-center justify-between w-full">
+                <header className="flex items-center justify-between w-full my-6">
                     <h2 className="text-4xl font-bold">Employees</h2>
 
                     {/* SEARCH AND ADD BUTTON */}
                     <div className="flex items-center h-[30px] space-x-2">
                         {/* SEARCH BAR */}
                         <div className="w-[200px] h-full flex rounded-md border-solid border-black border-2  justify-between bg-offWhite px-1 items-center">
-                            <input type="text" placeholder="search..." className="rounded-lg w-3/5 bg-offWhite flex-grow" />
+                            <input type="text" placeholder="search..." className="rounded-lg w-3/5 bg-offWhite flex-grow ring-transparent border-opacity-0 border-white" />
                             <label className="h-[25px] w-3">
                                 <img src={searchIcon} alt="search icon" className="h-full w-full" />
                             </label>
@@ -26,6 +26,51 @@ function Employees() {
                         </button>
                     </div>
                 </header>
+
+                {/* TABLE HERE */}
+                <table className="table-auto w-full border border-black">
+                    <thead className="bg-ming text-white [&>*]:border [&>*]:border-black">
+                        <tr className="[&>*]:border [&>*]:border-black">
+                            <th className="justify-items-start">Name</th>
+                            <th>Email</th>
+                            <th>Job position</th>
+                            <th>Payroll</th>
+                        </tr>
+                    </thead>
+                    <tbody className=" [&>*]:border [&>*]:border-black">
+                        <tr className="border border-black [&>*]:border [&>*]:border-black">
+                            <th className="border border-black">Khodor</th>
+                            <th className="border border-black">Khodorhassoun@outlook.com</th>
+                            <th>Project Manager</th>
+                            <th>3000</th>
+                        </tr>
+                        <tr className="bg-beau [&>*]:border [&>*]:border-black">
+                            <th>Khodor</th>
+                            <th>Khodorhassoun@outlook.com</th>
+                            <th>Project Manager</th>
+                            <th>3000</th>
+                        </tr>
+                        <tr className="[&>*]:border [&>*]:border-black">
+                            <th>Khodor</th>
+                            <th>Khodorhassoun@outlook.com</th>
+                            <th>Project Manager</th>
+                            <th>3000</th>
+                        </tr>
+                        <tr className="[&>*]:border [&>*]:border-black bg-beau">
+                            <th>Khodor</th>
+                            <th>Khodorhassoun@outlook.com</th>
+                            <th>Project Manager</th>
+                            <th>3000</th>
+                        </tr>
+                        <tr className="[&>*]:border [&>*]:border-black">
+                            <th>Khodorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr</th>
+                            <th>Khodorhassoun@outlook.com</th>
+                            <th>Project Manager</th>
+                            <th>3000</th>
+                        </tr>
+                    </tbody>
+
+                </table>
             </section>
         </section>
     )
