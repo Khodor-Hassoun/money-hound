9da4095fea529782a23d2438c67367236ba43e15
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-07 11:19:40 +0200

    Frontend: design navbar footer

--- frontend/webapp/src/components/Navbar.jsx
+++ frontend/webapp/src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import dots from "../resources/images/icons8-more-24.png"
 function Navbar() {
     const isActive = false
     return (
-        <nav className="h-screen w-[350px] bg-ming text-white flex flex-col">
+        //justify-between
+        <nav className="h-screen w-[350px] bg-ming text-white flex flex-col justify-between">
             {/* COMPANY LOGO, NAME AND COMPANY SETTINGS */}
             <div className="flex items-center w-full pt-6 justify-between px-4">
                 {/* COMPANY LOGO AND NAME */}
@@ -18,7 +19,7 @@ function Navbar() {
                     <h2 className="text-xl ml-3">Company name</h2>
                 </div>
                 {/* COMPANY SETTINGS FOR OWNER */}
-                <div className="">
+                <div className="cursor-pointer">
                     <img src={dots} alt="options" />
                 </div>
             </div>
@@ -45,6 +46,25 @@ function Navbar() {
                 <div className="flex h-[50px] items-center bg-ming brightness-100 hover:brightness-110 cursor-pointer first:active:bg-tangerine last:focus:text-tangerine">
                     <div className="h-full bg-white w-[12px] active:bg-tangerine " />
                     <p className=" w-full ml-8">Insights</p>
+                </div>
+            </div>
+
+            {/* NAME AND SIGNOUT BUTTON */}
+            <div className="pb-6 w-full px-4">
+                {/* NAME AND OPTIONS */}
+                <div className="flex w-full justify-between">
+                    <h2>Khodor Hassoun</h2>
+                    <div className="cursor-pointer">
+                        <img src={dots} alt="options" />
+                    </div>
+                </div>
+                {/* SIGNOUT BUTTON */}
+                <button
+                    className="bg-tangerine text-white my-4 p-2 rounded-xl w-full cursor-pointer">
+                    SIGN UP
+                </button>
+                <div>
+
                 </div>
             </div>
         </nav>
