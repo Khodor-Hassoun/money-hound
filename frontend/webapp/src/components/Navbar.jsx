fac7fd5604c1528a815f2a20da9f7ea85d0cc190
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-07 10:57:54 +0200

    Frontend: reduce lightbar height

--- frontend/webapp/src/components/Navbar.jsx
+++ frontend/webapp/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import logo from "../resources/images/seanpollockPhYq704ffdAunsplash.jpg"
 import dots from "../resources/images/icons8-more-24.png"
 function Navbar() {
+    const isActive = false
     return (
-        <nav className="h-screen w-[350px] bg-ming text-white">
+        <nav className="h-screen w-[350px] bg-ming text-white flex flex-col">
             {/* COMPANY LOGO, NAME AND COMPANY SETTINGS */}
             <div className="flex items-center w-full pt-6 justify-between px-4">
                 {/* COMPANY LOGO AND NAME */}
@@ -25,16 +26,25 @@ function Navbar() {
             {/* OPTIONS LINKS */}
             <div className="space-y-6 ">
                 {/* LINK 1 */}
-                <div className="flex h-[70px] items-center bg-ming brightness-110">
+                <div className="flex h-[50px] items-center bg-ming brightness-110">
                     {/* Lightbar */}
                     <div className="h-full bg-tangerine w-[12px]" />
                     <p className="text-tangerine w-full ml-8">Dashboard</p>
                 </div>
                 {/* LINK 2 */}
-                <div className="flex h-[70px] items-center bg-ming brightness-100 hover:brightness-110 cursor-pointer">
-                    {/* Lightbar */}
-                    <div className="h-full bg-white w-[12px]" />
-                    <p className=" w-full ml-8">Dashboard</p>
+                <div className="flex h-[50px] items-center bg-ming brightness-100 hover:brightness-110 cursor-pointer first:active:bg-tangerine last:focus:text-tangerine">
+                    <div className="h-full bg-white w-[12px] active:bg-tangerine" />
+                    <p className=" w-full ml-8">Employees</p>
+                </div>
+                {/* LINK 3 */}
+                <div className="flex h-[50px] items-center bg-ming brightness-100 hover:brightness-110 cursor-pointer first:active:bg-tangerine last:focus:text-tangerine">
+                    <div className="h-full bg-white w-[12px] active:bg-tangerine" />
+                    <p className=" w-full ml-8">Projects</p>
+                </div>
+                {/* LINK 4 */}
+                <div className="flex h-[50px] items-center bg-ming brightness-100 hover:brightness-110 cursor-pointer first:active:bg-tangerine last:focus:text-tangerine">
+                    <div className="h-full bg-white w-[12px] active:bg-tangerine " />
+                    <p className=" w-full ml-8">Insights</p>
                 </div>
             </div>
         </nav>
