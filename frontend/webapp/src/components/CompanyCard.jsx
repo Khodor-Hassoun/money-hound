8bc8e9c4e7ef0ee8fd6e5110ebbc51cfb75a3502
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-11 15:21:52 +0200

    Frontend: small update of company card

--- frontend/webapp/src/components/CompanyCard.jsx
+++ frontend/webapp/src/components/CompanyCard.jsx
@@ -3,27 +3,26 @@ import logo from "../resources/images/Your-Logo-here.png"
 function CompanyCard() {
     return (
         <>
-            <section className="bg-ming flex items-center justify-center h-screen">
-                <div className="bg-offWhite rounded-xl flex w-1/4 items-center space-x-5 p-2 shadow-xl hover:p-3 hover:shadow-2xl">
-                    <div>
-                        <div className="h-[60px] w-[60px] bg-white flex justify-center items-center rounded-xl">
-                            <div className="h-[50px] w-[50px] rounded-xl">
-                                <label htmlFor="hidden-input">
-                                    <img src={logo} alt="logo" className="h-full w-full rounded-xl" />
-                                    {/* <img src={logo} alt="logo" className="h-full w-full rounded-xl" /> */}
-                                </label>
-                                <input type="file" className="invisible" id="hidden-input" />
-                            </div>
+            <div className="bg-beau rounded-xl flex w-1/4 items-center space-x-5 p-2 shadow-xl hover:p-3 hover:shadow-2xl">
+                <div>
+                    <div className="h-[60px] w-[60px] bg-white flex justify-center items-center rounded-xl">
+                        <div className="h-[50px] w-[50px] rounded-xl">
+                            <label htmlFor="hidden-input">
+                                <img src={logo} alt="logo" className="h-full w-full rounded-xl" />
+                                {/* <img src={logo} alt="logo" className="h-full w-full rounded-xl" /> */}
+                            </label>
+                            <input type="file" className="invisible" id="hidden-input" />
                         </div>
                     </div>
-                    <div className="flex flex-col space-x-5">
-                        <h2 className="text-2xl">Company Name</h2>
+                </div>
+                <div className="flex flex-col justify-between">
+                    <h2 className="text-2xl">Company Name</h2>
+                    <span>Owner</span>
 
-
-                    </div>
 
                 </div>
-            </section>
+
+            </div>
         </>
     )
 }
