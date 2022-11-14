3dcdb76fb3f7e690af33ba343bad9558b228d87a
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-15 00:10:28 +0200

    Frontend: manage overflow of projects

--- frontend/webapp/src/pages/Projects/Projects.jsx
+++ frontend/webapp/src/pages/Projects/Projects.jsx
@@ -4,7 +4,7 @@ function Projects() {
     return (
         <section className="flex bg-offWhite pr-4">
             <Navbar />
-            <section className="flex-grow">
+            <section className="flex-grow max-h-screen overflow-auto">
                 <header className="flex items-center justify-between w-full my-6">
                     <h2 className="text-4xl font-bold">Projects</h2>
 
@@ -23,9 +23,9 @@ function Projects() {
                     </div>
                 </header>
                 {/* CONTENT CONTAINER */}
-                <div className="flex">
+                <div className="grid grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6">
                     {/* CARD */}
-                    <div className="p-4 bg-white shadow-lg rounded-2xl w-7/12 lg:w-1/3">
+                    <div className="p-4 bg-white shadow-lg rounded-2xl">
                         {/* CARD TITLE AND MANAGER */}
                         <div className="flex flex-col mb-3">
                             <h2 className="text-xl font-bold">Project Name</h2>
@@ -53,8 +53,174 @@ function Projects() {
                             </ul>
                         </div>
                     </div>
-
-
+                    <div className="p-4 bg-white shadow-lg rounded-2xl">
+                        {/* CARD TITLE AND MANAGER */}
+                        <div className="flex flex-col mb-3">
+                            <h2 className="text-xl font-bold">Project Name</h2>
+                            <span>Project manager: John Smith</span>
+                        </div>
+                        {/* MONEY BAR, BUDGET, SPENT */}
+                        <div className="my-4">
+                            {/* MONEY BAR */}
+                            <div className="w-full bg-tea h-[40px]">
+                                <div className="w-[70%] bg-mint h-full">
+                                </div>
+                            </div>
+                            {/* BUDGET */}
+                            <div className="flex justify-between w-full">
+                                <p>Budget: 1000$</p>
+                                <p>Money Spent: 7000$</p>
+                            </div>
+                        </div>
+                        <div className="py-2 space-y-2">
+                            <p className="font-bold">Recent activity:</p>
+                            <ul className="list-disc list-inside space-y-1">
+                                <li>Activity</li>
+                                <li>Activity</li>
+                                <li>Activity</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="p-4 bg-white shadow-lg rounded-2xl">
+                        {/* CARD TITLE AND MANAGER */}
+                        <div className="flex flex-col mb-3">
+                            <h2 className="text-xl font-bold">Project Name</h2>
+                            <span>Project manager: John Smith</span>
+                        </div>
+                        {/* MONEY BAR, BUDGET, SPENT */}
+                        <div className="my-4">
+                            {/* MONEY BAR */}
+                            <div className="w-full bg-tea h-[40px]">
+                                <div className="w-[70%] bg-mint h-full">
+                                </div>
+                            </div>
+                            {/* BUDGET */}
+                            <div className="flex justify-between w-full">
+                                <p>Budget: 1000$</p>
+                                <p>Money Spent: 7000$</p>
+                            </div>
+                        </div>
+                        <div className="py-2 space-y-2">
+                            <p className="font-bold">Recent activity:</p>
+                            <ul className="list-disc list-inside space-y-1">
+                                <li>Activity</li>
+                                <li>Activity</li>
+                                <li>Activity</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="p-4 bg-white shadow-lg rounded-2xl">
+                        {/* CARD TITLE AND MANAGER */}
+                        <div className="flex flex-col mb-3">
+                            <h2 className="text-xl font-bold">Project Name</h2>
+                            <span>Project manager: John Smith</span>
+                        </div>
+                        {/* MONEY BAR, BUDGET, SPENT */}
+                        <div className="my-4">
+                            {/* MONEY BAR */}
+                            <div className="w-full bg-tea h-[40px]">
+                                <div className="w-[70%] bg-mint h-full">
+                                </div>
+                            </div>
+                            {/* BUDGET */}
+                            <div className="flex justify-between w-full">
+                                <p>Budget: 1000$</p>
+                                <p>Money Spent: 7000$</p>
+                            </div>
+                        </div>
+                        <div className="py-2 space-y-2">
+                            <p className="font-bold">Recent activity:</p>
+                            <ul className="list-disc list-inside space-y-1">
+                                <li>Activity</li>
+                                <li>Activity</li>
+                                <li>Activity</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="p-4 bg-white shadow-lg rounded-2xl">
+                        {/* CARD TITLE AND MANAGER */}
+                        <div className="flex flex-col mb-3">
+                            <h2 className="text-xl font-bold">Project Name</h2>
+                            <span>Project manager: John Smith</span>
+                        </div>
+                        {/* MONEY BAR, BUDGET, SPENT */}
+                        <div className="my-4">
+                            {/* MONEY BAR */}
+                            <div className="w-full bg-tea h-[40px]">
+                                <div className="w-[70%] bg-mint h-full">
+                                </div>
+                            </div>
+                            {/* BUDGET */}
+                            <div className="flex justify-between w-full">
+                                <p>Budget: 1000$</p>
+                                <p>Money Spent: 7000$</p>
+                            </div>
+                        </div>
+                        <div className="py-2 space-y-2">
+                            <p className="font-bold">Recent activity:</p>
+                            <ul className="list-disc list-inside space-y-1">
+                                <li>Activity</li>
+                                <li>Activity</li>
+                                <li>Activity</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="p-4 bg-white shadow-lg rounded-2xl">
+                        {/* CARD TITLE AND MANAGER */}
+                        <div className="flex flex-col mb-3">
+                            <h2 className="text-xl font-bold">Project Name</h2>
+                            <span>Project manager: John Smith</span>
+                        </div>
+                        {/* MONEY BAR, BUDGET, SPENT */}
+                        <div className="my-4">
+                            {/* MONEY BAR */}
+                            <div className="w-full bg-tea h-[40px]">
+                                <div className="w-[70%] bg-mint h-full">
+                                </div>
+                            </div>
+                            {/* BUDGET */}
+                            <div className="flex justify-between w-full">
+                                <p>Budget: 1000$</p>
+                                <p>Money Spent: 7000$</p>
+                            </div>
+                        </div>
+                        <div className="py-2 space-y-2">
+                            <p className="font-bold">Recent activity:</p>
+                            <ul className="list-disc list-inside space-y-1">
+                                <li>Activity</li>
+                                <li>Activity</li>
+                                <li>Activity</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="p-4 bg-white shadow-lg rounded-2xl">
+                        {/* CARD TITLE AND MANAGER */}
+                        <div className="flex flex-col mb-3">
+                            <h2 className="text-xl font-bold">Project Name</h2>
+                            <span>Project manager: John Smith</span>
+                        </div>
+                        {/* MONEY BAR, BUDGET, SPENT */}
+                        <div className="my-4">
+                            {/* MONEY BAR */}
+                            <div className="w-full bg-tea h-[40px]">
+                                <div className="w-[70%] bg-mint h-full">
+                                </div>
+                            </div>
+                            {/* BUDGET */}
+                            <div className="flex justify-between w-full">
+                                <p>Budget: 1000$</p>
+                                <p>Money Spent: 7000$</p>
+                            </div>
+                        </div>
+                        <div className="py-2 space-y-2">
+                            <p className="font-bold">Recent activity:</p>
+                            <ul className="list-disc list-inside space-y-1">
+                                <li>Activity</li>
+                                <li>Activity</li>
+                                <li>Activity</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </section>
         </section>
