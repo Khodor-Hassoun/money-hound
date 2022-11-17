5a647a8f89fd8c84d8fdc9a4ae383dbd2618217c
Khodor Hassoun <khodorhassoun@outlook.com>
2022-11-17 21:49:11 +0200

    Frontend: get company expenses

--- frontend/webapp/src/pages/Insights/Insights.jsx
+++ frontend/webapp/src/pages/Insights/Insights.jsx
@@ -8,19 +8,48 @@ function Insights() {
     const user = useSelector(state => state.user)
     const [revenues, setRevenues] = useState([])
     const [expenses, setExpenses] = useState([])
+    const [employees, setEmployees] = useState([])
+    // GET REVENUES
     useEffect(() => {
         axios.get("http://localhost:3002/company/revenues", {
             headers: {
                 authorization: `Bearer ${user.token}`
             },
         }).then(res => {
-            console.log(res)
+            setRevenues(res.data)
+        }).catch(e => {
+            console.log(e)
         })
     }, [])
+    // GET EXPENSES
+    useEffect(() => {
+        axios.get("http://localhost:3002/company/expenses", {
+            headers: {
+                authorization: `Bearer ${user.token}`
+            },
+        }).then(res => {
+            setExpenses(res.data)
+        }).catch(e => {
+            console.log(e)
+        })
+    }, [])
+    // GET EMPLOYEES TO GET THEIR PAY
+    useEffect(() => {
+        axios.get("http://localhost:3002/company/employees", {
+            headers: {
+                authorization: `Bearer ${user.token}`
+            },
+        }).then(res => {
+            console.log(res.data)
+            // setEmployees(res.data.employees)
+            setEmployees(res.data.employees)
+        })
+    }, [])
+    // ADD C
     return (
         <section className="flex bg-offWhite pr-4">
             <Navbar />
-
+            <button onClick={() => { console.log(expenses); console.log(revenues) }}>Press heeeeeeeeeeeeee</button>
         </section>
 
     )
